6654b57726f10f5b59f0f43d5f8a9a67d4fca60c
Nadia Araujo <nadiaaraujo1@gmail.com>
2022-06-11 18:57:32 -0300

    Criação do projeto

---
+++ src/main/java/com/estudojava/workshop/ProjetojavaJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.estudojava.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetojavaJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetojavaJpaHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudojava/workshop/ProjetojavaJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudojava.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetojavaJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
