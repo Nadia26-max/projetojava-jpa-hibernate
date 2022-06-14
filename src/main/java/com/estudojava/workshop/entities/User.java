9a3d9adfa3c29af95dd83093d4986bd6f402010f
Nadia Araujo <nadiaaraujo1@gmail.com>
2022-06-14 11:37:22 -0300

    User entities e resources

---
+++ src/main/java/com/estudojava/workshop/entities/User.java
@@ -0,0 +1,75 @@
+package com.estudojava.workshop.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private long id;
+	private String nome;
+	private String fone;
+	private String senha;
+	
+	public User() {
+	}
+
+	public User(long id, String nome, String fone, String senha) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.fone = fone;
+		this.senha = senha;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getFone() {
+		return fone;
+	}
+
+	public void setFone(String fone) {
+		this.fone = fone;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	//Comparação de dois objetos
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return id == other.id;
+	}
+}
---
+++ src/main/java/com/estudojava/workshop/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.estudojava.workshop.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.estudojava.workshop.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")//Caminho
+public class UserResource {
+
+	//Método para acessar os usuarios (retornar respostas de requisição web)
+	@GetMapping
+	public ResponseEntity<User> findAll(){//Vai retornar os usuarios
+		User u = new User(1L, "Mary", "90909090","12345");
+		return ResponseEntity.ok().body(u);//Retornar com o sucesso no corpo da resposta o usuario u que foi instanciado
+	}
+}
