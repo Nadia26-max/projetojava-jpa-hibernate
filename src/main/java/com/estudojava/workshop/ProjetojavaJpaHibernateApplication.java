4383bdad131179f56ebf87516b448c3fa0057a73
Nadia Araujo <nadiaaraujo1@gmail.com>
2022-06-30 12:00:01 -0300

    Configurando conexão com o PostgreSQL

--- src/main/java/com/estudojava/workshop/ProjetojavaJpaHibernateApplication.java
+++ src/main/java/com/estudojava/workshop/ProjetojavaJpaHibernateApplication.java
@@ -2,10 +2,8 @@ package com.estudojava.workshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication
-@EntityScan(basePackages = "com.estudojava.workshop.entities")
+@SpringBootApplication(scanBasePackages = "com.estudojava.workshop.entities")
 public class ProjetojavaJpaHibernateApplication {
 
 	public static void main(String[] args) {
